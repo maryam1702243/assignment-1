1137a341b305d9c3c33bf4373cf42199bfcb3623
maryam1702243 <maryam.chaiwala.sod24@anu.edu.in>
2025-09-16 14:10:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  background('#87CEEB'); 
+  noStroke();
+
+  
+  fill('#D2B48C'); // 
+  triangle(200, 300, 170, 200, 230, 200);
+
+  
+fill('pink');
+  circle(200, 170, 80);
+
+  fill('red');
+  circle(200, 120, 20);
+}
